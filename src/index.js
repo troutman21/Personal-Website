3cd78c3a90e431a0159650d9ff4a2237c3e6625b
Phillip Troutman <philliptroutman21@gmail.com>
2019-02-16 14:52:16 -0500

    added gitIgnore file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import App from './components/app';
+
+//inject or app into our html root element
+reactDOM.render(<App/>, document.querySelector('#root'));
