3ac52fdd9857683d1af8447b129c100bc3aca617
Phillip Troutman <philliptroutman21@gmail.com>
2019-02-26 07:45:13 -0500

    added mobile friendly css

--- src/components/header.js
+++ src/components/header.js
@@ -8,10 +8,12 @@ class Header extends Component{
       <nav>
         <ul>
           <li id='name'><NavLink exact={true}  activeClassName='selected' to='/'>Phillip_Troutman</NavLink></li>
+          <div id='sub-nav'>
           <li><NavLink activeClassName='selected' to='/speaking'>_speaking</NavLink></li>
           <li><a href='mailto:philliptroutman21@gmail.com'>_email</a></li>
           <li><a href='https://github.com/troutman21'>_programming</a></li>
           <li><a href='https://twitter.com/_Troutman_'>_twitter</a></li>
+          </div>
         </ul>
       </nav>
     </header>
