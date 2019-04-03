f0fd57f3fe3de6fc4849bbc4d28d6ef5f6394abf
Phillip Troutman <philliptroutman21@gmail.com>
2019-04-03 16:19:44 -0400

    fixed typo

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,7 +5,7 @@ class Bio extends Component{
     return(
       <div className='bio'>
         <div>
-        My name is Phillip Troutman.  I'm a Senior Full Stack Engineer and Lead Instructor at <a href='https://www.codesmith.io'>Codesmith</a> NY. Codesmith is a 12 week software engineering residency focused on building autonomies problem solving engineers.
+        My name is Phillip Troutman.  I'm a Senior Full Stack Engineer and Lead Instructor at <a href='https://www.codesmith.io'>Codesmith</a> NY. Codesmith is a 12 week software engineering residency focused on building autonomous problem solving engineers.
         </div>
         <div>
           My day to day is spent building scalable  React/Node applications and managing a team of engineers. I’m especially passionate about advanced React features, declarative programming, and arguing the merits of vanilla Javascript over jQuery. At Codesmith I give technical lectures on classic computer science theory and all things modern web development, including data structures, time complexity, front-end fundamentals, React, Node, Dev-Ops, AJAX, Asynchronous Javascript, etc.
